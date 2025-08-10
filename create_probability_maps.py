495969db3c3db0f7e9fdc19461b8a3f2aa45d009
Pablo Merchán-Rivera <pmr.prod@outlook.com>
2025-08-10 20:34:30 +0200

    Remove redundant introductory comment from create_probability_maps.py

--- create_probability_maps.py
+++ create_probability_maps.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Comprehensive script to:
 1. Extract post-convergence parameters from DREAM results
 2. Run GWM model for each parameter set
 3. Save all head values for each run  
